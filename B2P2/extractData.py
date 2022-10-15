2bde061d16c62ace82ce1c6dbb89a0a10bec698d
skinnymega <77574684+skinnymega@users.noreply.github.com>
2022-10-15 18:59:38 -0300

    reading file and turning it to dict with xmltodict

--- B2P2/extractData.py
+++ B2P2/extractData.py
@@ -1,1 +1,12 @@
-import pandas as pd
+import xml.etree.ElementTree as ET
+# https://www.geeksforgeeks.org/xml-parsing-python/
+import xmltodict
+# https://docs.python-guide.org/scenarios/xml/
+
+PATH = "C:/Users/alexa/Desktop/"
+FILEPATH = PATH + "cci36lab2.dae"
+
+
+# turn xml from file to dict
+with open(FILEPATH) as fd:
+    doc = xmltodict.parse(fd.read())
